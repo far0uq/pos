1bbf125a05c2c4c0f37a1c765c917620806209d8
far0uq <farouq.swe@gmail.com>
2024-08-30 20:09:52 +0500

    update: refactoring auth related code
    Refactored the code to be consistent across files. Removed test files. Fixed up API's, middleware and pages.
    
    Next todo is AntD ConfigProvider setup.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,13 +4,17 @@ import React, { useEffect } from "react";
 import { Form, Input, Button, Flex, Space } from "antd";
 import Item from "antd/lib/list/Item";
 import { useSearchParams, useRouter } from "next/navigation";
-import { getAuthURL } from "../clientAPI/authAPI";
+import {
+  getAuthURL,
+  handleCookiefication,
+  handleGetAndSetToken,
+  handleObtainToken,
+} from "../clientAPI/authAPI";
 
 function AuthForm() {
   const handleSubmit = async () => {
     try {
       const url = await getAuthURL();
-      console.log(url);
       window.open(url);
     } catch (error) {
       console.error("Error: ", error);
@@ -19,46 +23,20 @@ function AuthForm() {
 
   const params = useSearchParams();
   const router = useRouter();
-  useEffect(() => {
-    const executeObtainToken = async () => {
-      if (params.get("code")) {
-        const authCode = params.get("code") as string;
-        console.log(authCode);
-        console.log(localStorage.getItem("code_verifier"));
-        try {
-          const resp = await fetch("/api/authTokenAPI", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              codeVerifier: localStorage.getItem("code_verifier"),
-              authenticationCode: authCode,
-            }),
-          });
-          if (resp.status === 200) {
-            const data = await resp.json();
-            const cookie_resp = await fetch("/api/cookiefy", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                accessToken: data.accessToken,
-                refreshToken: data.refreshToken,
-                expiresAt: data.expiresAt,
-                merchantId: data.merchantId,
-              }),
-            });
 
-            if (cookie_resp.status === 200) router.push("/mainpage");
-          }
-        } catch (error) {
-          console.error("Error: ", error);
-        }
+  const getParamAndCall = async () => {
+    if (params.get("code")) {
+      const authCode = params.get("code") as string;
+      const resp = await handleGetAndSetToken(authCode);
+      if (resp.status === 200) {
+        console.log("Success");
+        router.push("/mainpage");
       }
-    };
-    executeObtainToken();
+    }
+  };
+
+  useEffect(() => {
+    getParamAndCall();
   }, []);
 
   return (
--- src/app/clientAPI/authAPI.ts
+++ src/app/clientAPI/authAPI.ts
@@ -1,28 +1,97 @@
+"use client";
 import pckeChallenge from "pkce-challenge";
 
 export async function getAuthURL() {
-  let local_code_verifier = localStorage.getItem("code_verifier");
-  let local_code_challenge = localStorage.getItem("code_challenge");
+  let codeVerifier = localStorage.getItem("code_verifier");
+  let codeChallenge = localStorage.getItem("code_challenge");
 
-  if (!local_code_challenge && !local_code_verifier) {
+  const challengeInfoExists = codeVerifier && codeChallenge;
+
+  if (!challengeInfoExists) {
     let { code_verifier, code_challenge } = await pckeChallenge();
 
-    local_code_challenge = code_challenge;
-    local_code_verifier = code_verifier;
+    codeVerifier = code_verifier;
+    codeChallenge = code_challenge;
 
-    localStorage.setItem("code_verifier", code_verifier);
-    localStorage.setItem("code_challenge", code_challenge);
-
-    console.log("code_challenge: ", local_code_challenge);
-    console.log("code_verifier: ", local_code_verifier);
+    localStorage.setItem("code_verifier", codeVerifier);
+    localStorage.setItem("code_challenge", codeChallenge);
   }
 
-  const redirect_uri = "http://localhost:3000/auth";
-  const client_id = "sq0idp-tDr6r_tlpCjHD9tDQrV8mg";
+  const redirectURI = "http://localhost:3000/auth";
+  const clientID = "sq0idp-tDr6r_tlpCjHD9tDQrV8mg";
 
-  const url = `https://connect.squareup.com/oauth2/authorize?client_id=${client_id}&scope=MERCHANT_PROFILE_READ&session=false&redirect_uri=${redirect_uri}&code_challenge=${local_code_challenge}`;
+  const url = `https://connect.squareup.com/oauth2/authorize?client_id=${clientID}&scope=MERCHANT_PROFILE_READ&session=false&redirect_uri=${redirectURI}&code_challenge=${codeChallenge}`;
 
   console.log(url);
-
   return url;
+}
+
+export async function handleObtainToken(authCode: string) {
+  try {
+    if (authCode) {
+      const resp = await fetch("/api/authTokenAPI", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          codeVerifier: localStorage.getItem("code_verifier"),
+          authenticationCode: authCode,
+        }),
+      });
+
+      if (resp.status == 200) {
+        const data = await resp.json();
+        return { data, status: resp.status };
+      } else {
+        throw new Error("authTokenAPI failed.");
+      }
+    } else {
+      throw new Error("No authCode provided.");
+    }
+  } catch (error) {
+    console.error(error);
+    return { data: error, status: 500 };
+  }
+}
+
+export async function handleCookiefication(data: any) {
+  try {
+    const { accessToken, refreshToken, expiresAt, merchantId } = data;
+    const resp = await fetch("/api/cookiefy", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        accessToken,
+        refreshToken,
+        expiresAt,
+        merchantId,
+      }),
+    });
+    if (resp.status == 200) return { status: resp.status };
+    else {
+      throw new Error("Error in cookiefication");
+    }
+  } catch (error) {
+    console.log(error);
+    return { data: error, status: 500 };
+  }
+}
+
+export async function handleGetAndSetToken(authCode: string) {
+  try {
+    const resp = await handleObtainToken(authCode);
+    if (resp.status === 200) {
+      const cookieResp = await handleCookiefication(resp.data);
+      if (cookieResp.status === 200) return { status: cookieResp.status };
+      else throw new Error("Check cookiefication API for Errors.");
+    } else {
+      throw new Error("Check obtainToken API for Errors.");
+    }
+  } catch (error) {
+    console.error(error);
+    return { status: 500 };
+  }
 }
