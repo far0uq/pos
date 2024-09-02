c7c4bbbf10d81467669b157372a0e7d348fc2261
far0uq <farouq.swe@gmail.com>
2024-09-02 20:32:22 +0500

    feat: created api for searching particular products

--- src/app/api/productsAPI/route.ts
+++ src/app/api/productsAPI/route.ts
@@ -14,23 +14,8 @@ export async function GET() {
     const resp = await client.catalogApi.listCatalog();
     const objects = resp.result.objects;
 
-    const cleanedObjects: Product[] = [];
     if (objects) {
-      objects.forEach((object) => {
-        const cleanedPrice = object.itemData?.variations?.[0]?.itemVariationData
-          ?.priceMoney?.amount
-          ? Number(
-              object.itemData.variations[0].itemVariationData.priceMoney.amount
-            ) / 100
-          : 0;
-        cleanedObjects.push({
-          id: object.id,
-          name: object.itemData?.name as string,
-          price: cleanedPrice,
-          image: "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b",
-        });
-      });
-
+      const cleanedObjects = cleanProductObjects(objects);
       return NextResponse.json({ result: cleanedObjects }, { status: 200 });
     } else {
       return NextResponse.json({ result: [] }, { status: 200 });
@@ -40,3 +25,43 @@ export async function GET() {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
+
+export async function POST(req: Request) {
+  try {
+    const { query } = req.json();
+    const client = new Client({
+      environment: Environment.Production,
+      accessToken: process.env.NEXT_SERVER_ACCESS_TOKEN_PROD,
+    });
+
+    const resp = await client.catalogApi.searchCatalogItems({
+      textFilter: query,
+    });
+    const objects = resp.result.items;
+    if (objects) {
+      const cleanedObjects = cleanProductObjects(objects);
+      return NextResponse.json({ result: cleanedObjects }, { status: 200 });
+    } else {
+      return NextResponse.json({ result: [] }, { status: 200 });
+    }
+  } catch (error) {}
+}
+
+const cleanProductObjects = (objects: CatalogObject[]): Product[] => {
+  const cleanedObjects: Product[] = [];
+  objects.forEach((object) => {
+    const cleanedPrice = object.itemData?.variations?.[0]?.itemVariationData
+      ?.priceMoney?.amount
+      ? Number(
+          object.itemData.variations[0].itemVariationData.priceMoney.amount
+        ) / 100
+      : 0;
+    cleanedObjects.push({
+      id: object.id,
+      name: object.itemData?.name as string,
+      price: cleanedPrice,
+      image: "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b",
+    });
+  });
+  return cleanedObjects;
+};
