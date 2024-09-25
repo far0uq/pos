c35f8c5bcedb5f462ce897dbfabea0e2310652eb
far0uq <farouq.swe@gmail.com>
2024-09-25 19:32:30 +0500

    test: cartcontainer ~ added testing when the order is successfully calculated

--- __tests__/CartContainer.test.tsx
+++ __tests__/CartContainer.test.tsx
@@ -97,53 +97,58 @@ describe("Cart Container", () => {
     expect(useMutation).toHaveBeenCalled();
   });
 
-  //   it("should show total amount when order is calculated successfully", () => {
-  //     (useMutation as jest.Mock).mockImplementationOnce(() => ({
-  //       data: {
-  //         orderResponse: {
-  //           totalMoney: 100,
-  //           totalTaxMoney: 10,
-  //           totalDiscountMoney: 10,
-  //         },
-  //       },
-  //       error: null,
-  //       isError: false,
-  //       isPending: false,
-  //       isSucessful: true,
-  //       mutate: jest.fn().mockReturnValue(() => ({
-  //         orderResponse: {
-  //           totalMoney: 100,
-  //           totalTaxMoney: 10,
-  //           totalDiscountMoney: 10,
-  //         },
-  //       })),
-  //     }));
+  it("should show total amount when order is calculated successfully", () => {
+    (useMutation as jest.Mock).mockImplementationOnce(() => ({
+      data: {
+        orderResponse: {
+          totalMoney: 100,
+          totalDiscountMoney: 10,
+          totalTaxMoney: 5,
+        },
+        lineItemDetails: [],
+      },
+      error: null,
+      isError: false,
+      isPending: false,
+      isSucessful: true,
+      mutate: jest.fn(),
+    }));
 
-  //     const product = {
-  //       id: "1",
-  //       name: "Dummy Item",
-  //       image:
-  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjw-x2av0YFJfxJx6oN6lOQqC3TxftSOqtKA&s",
-  //       price: 10,
-  //     };
+    const product = {
+      id: "1",
+      name: "Dummy Item",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjw-x2av0YFJfxJx6oN6lOQqC3TxftSOqtKA&s",
+      price: 10,
+    };
 
-  //     let store = useTotalStore.getState();
-  //     store.addProduct(product);
+    let store = useTotalStore.getState();
+    store.addProduct(product);
 
-  //     const queryClient = new QueryClient();
-  //     let { container } = render(
-  //       <QueryClientProvider client={queryClient}>
-  //         <CartContainer />
-  //       </QueryClientProvider>
-  //     );
+    const queryClient = new QueryClient();
+    let { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <CartContainer />
+      </QueryClientProvider>
+    );
 
-  //     const totalMoneyElement =
-  //       container.getElementsByClassName("total-money")[0];
+    const totalMoneyPara = container.getElementsByClassName("total-money")[0];
 
-  //     console.log(totalMoneyElement);
+    const orderErrorText = "Error calculating order";
+    const orderLoadingTest = "Calculating order...";
 
-  //     expect(totalMoneyElement).toHaveTextContent("$ 100");
-  //   });
+    const orderErrorElement =
+      container.getElementsByClassName(orderErrorText)[0] ?? null;
+    const orderLoadingElement =
+      container.getElementsByClassName(orderLoadingTest)[0] ?? null;
+    const orderEmptyElement =
+      container.getElementsByClassName("ant-empty")[0] ?? null;
+
+    expect(orderErrorElement).toBeNull();
+    expect(orderLoadingElement).toBeNull();
+    expect(orderEmptyElement).toBeNull();
+    expect(totalMoneyPara).toBeInTheDocument();
+  });
 
   it("should handle appropriately when order is being calculated", () => {
     (useMutation as jest.Mock).mockImplementationOnce(() => ({
